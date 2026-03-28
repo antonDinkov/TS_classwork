9d210187a71554ac28b148d0783983c2d1339876
antonDinkov <166150125+antonDinkov@users.noreply.github.com>
2026-03-28 15:15:48 +0700

    tuples

---
+++ tuples.ts
@@ -0,0 +1,10 @@
+// Tuples (fixed length + known types)
+let point: [number, number] = [10, 20];
+let rgba: [number, number, number, number] = [255, 0, 0, 0.5];
+console.log(point, rgba);
+// [ 10, 20 ] [ 255, 0, 0, 0.5 ]
+type TPerson = [string, number]; // [name, age]
+let bob: TPerson = ["Bob", 25];
+let carol: TPerson = ["Carol", 30];
+console.log(bob, carol);
+// [ 'Bob', 25 ] [ 'Carol', 30 ]
